90ad614b813e4fc2ffa82a740eea2cedd693a797
Ashley Kim Davis <davisak1@uci.edu>
2026-06-16 13:25:24 -0700

    reorganizing globals

--- static/timer.js
+++ static/timer.js
@@ -1,10 +1,11 @@
 let timer; 
 let focusMinutes = 25; 
-let timeLeft = focusMinutes * 60; 
+let breakMinutes = 5; 
+let focustimeLeft = focusMinutes * 60; 
+let breaktimeLeft = breakMinutes * 60; 
 let isRunning = false; 
-let mode = "focus"; //either focus or break, is the main idea here. 
-//basic globals 
-
+let mode = "focus"; //default as focus. 
+//globals. i feel like this might be the ugliest thing ever. 
 
 
 //make it compatible with html 
@@ -13,9 +14,11 @@ const startButton = document.getElementById('start');
 const pauseButton = document.getElementById('pause'); 
 const resetButton = document.getElementById('reset'); 
 
-const focusSelect = document.getElementById('focus-select'); //generally each go hand in hand. 
+
+const focusSelect = document.getElementById('focus-select'); 
 const breakSelect = document.getElementById('break-select');
 const ApplySettingsButton = document.getElementById('apply-settings'); 
+
 
 function updateDisplay(){
     const minutes = Math.floor(timeLeft / 60); 
@@ -30,55 +33,112 @@ resetButton.addEventListener('click', resetTimer);
 class Timer{
     timer(type){
         timer.type = type; 
-    }
+        //should also initialize the focus minutes and stuff 
+    }//revise this. 
 
     startTimer(){
         if (isRunning) return; 
          
         isRunning = true; 
         timer = setInterval(() => {
-            if (timeLeft > 0){
-                timeLeft--; 
+            if (focustimeLeft > 0){
+                focustimeLeft--; 
                 updateDisplay(); 
             }
             else{
                 clearInterval(timer); 
                 isRunning = false; 
-                alert("Timer has concluded. break time!")
-    
+                mode == "break"
             }
             }, 1000); 
-    
-        }
-        pauseTimer(){
-            clearInterval(timer); 
-            isRunning = false; 
         }
         
-        resetTimer(){
-            clearInterval(timer); 
-            isRunning = false; 
-            timeLeft = 25 * 60; 
+    breakTimer(){
+        if (mode == "break"){
+            if (breaktimeLeft > 0){
+                breaktimeLeft--; 
+                updateDisplay(); 
+            }
+            else{
+                clearInterval(timer); 
+                isRunning = false; 
+                mode == "focus"; 
+             }
             updateDisplay(); 
-        }
-
-    
-
-}
+            }
+     }
 
 
-function applySettings(){
-    if (isRunning){
-    alert("Cannot change timer settings while running."); 
+    pauseTimer(){
+        clearInterval(timer); 
+        isRunning = false; 
+    }
+        
+    resetTimer(){
+        clearInterval(timer); 
+        isRunning = false; 
+        timeLeft = 25 * 60; 
+        updateDisplay(); 
     }
 
-    focusMinutes = Number(focusSelect.value); 
-    breakMinutes = Number(breakSelect.value); 
 
-    mode = "focus"; 
-    timeLeft = focusMinutes * 60; 
-    updateDisplay(); 
+    applySettings(){
+        if (isRunning){
+        alert("Cannot change timer settings while running."); 
+        }
+
+        focusMinutes = Number(focusSelect.value); 
+        breakMinutes = Number(breakSelect.value); 
+
+        mode = "focus"; 
+        timeLeft = focusMinutes * 60; 
+        updateDisplay(); 
+    } 
+}//end bracket for class timer. 
+
+class classicPomo extends Timer{
+    constructor(){
+        super(); 
+        focusMinutes = 25; 
+        breakMinutes = 5; 
+
+    }
+
 }
 
+class burstPomo extends Timer{
+    constructor(){
+        super(); 
+        focusMinutes = 15; 
+        breakMinutes = 3; 
+    }
+}
 
+class DraugeimPomo extends Timer{
+    constructor(){
+        super(); 
+        focusMinutes = 52; 
+        breakMinutes = 17; 
+    }
+}
+
+class pomoThon extends Timer{
+    constructor(){
+        super(); 
+        focusMinutes = 90; 
+        breakMinutes = 25; 
+    }
+}
+
+class customPomo extends Timer{
+    constructor(){
+        super(); 
+        focusMinuntes = null; 
+        breakMinutes = null; 
+    }
+
+    assignFocusMinutes(focusMinutes){
+
+    }
+}
 updateDisplay(); 
